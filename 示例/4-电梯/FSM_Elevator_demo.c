5c8c389a0b7d54853d18c2989fd5d873f68ac7d4
Riggin <rigginli@icloud.com>
2024-10-18 10:04:21 +0800

    添加示例

--- 示例/4-电梯/FSM_Elevator_demo.c
+++ 示例/4-电梯/FSM_Elevator_demo.c
@@ -4,16 +4,25 @@
 
 /* standard lib */
 #include <stdio.h>  // printf #include <stdbool.h> // bool for _Bool and true for 1
-#include <conio.h>  // for clrscr, and getch()
-#include "stdlib.h" // for rand()
-#include "math.h"
+// #include <conio.h>  // for clrscr, and getch()
+#include <stdlib.h> // for rand()
+#include <math.h>
+
+static int event_idle(void);
+static int event_goingUp(void);
+static int event_goingDown(void);
+static int event_atTop(void);
+static int event_atBottom(void);
+static int event_malfunction(void);
+static int event_end(void);
+static int event_unexpected(void);
 
 // _idle 类似带下划线开头的对应state。  状态。
 // up    类似不带下划线的对应action。   动作。
 /* event array and enum below must be in sync! */
 enum state_codes { _idle=0, _goingUp=1, _goingDown=2, _AtTop=3, _AtBottom=4,  _malfunction=5, _unexpected=6, _end=7 };
 enum ret_codes   {   up=0,    down=1,      halt=2,      top=3,    bottom=4,       fail=5,         quit=6 };
-// int (* event[])(void) = { event_idle, event_goingUp, event_goingDown, event_atTop, event_atBottom, event_malfunction, event_unexpected, event_end };
+int (* event[])(void) = { event_idle, event_goingUp, event_goingDown, event_atTop, event_atBottom, event_malfunction, event_unexpected, event_end };
 
 int target_floor_number = 8;      // 目标楼层号。
 int current_floor_number = 1;     // 当前楼层号。
@@ -22,10 +31,10 @@ int accumulated_floor_number = 0; // 累计走了多少层。
 #define TOP_FLOOR    9 // 顶层号。
 #define BOTTOM_FLOOR 1 // 底层号。
 
-int event_idle(void){
+static int event_idle(void){
     printf("Idle!\n");
     printf("Input target floor number in [1, 9] (which floor you want to go?): \n");
-    target_floor_number = getch() - '0';
+    target_floor_number = getchar() - '0';
 
     if(current_floor_number < target_floor_number)
         return up;
@@ -34,7 +43,7 @@ int event_idle(void){
     else if(current_floor_number == target_floor_number)
         return halt;
 }
-int event_goingUp(void){
+static int event_goingUp(void){
     current_floor_number += 1;
     accumulated_floor_number += 1;
     printf("Going up! Floor number is %d\n", current_floor_number);
@@ -52,7 +61,7 @@ int event_goingUp(void){
     else
         return quit;
 }
-int event_goingDown(void){
+static int event_goingDown(void){
     current_floor_number -= 1;
     accumulated_floor_number += 1;
     printf("Going down! Floor number is %d\n", current_floor_number);
@@ -70,42 +79,39 @@ int event_goingDown(void){
     else
         return quit;
 }
-int event_atTop(void){
+static int event_atTop(void){
     printf("At top! current_floor_number= %d\n", current_floor_number);
     printf("Input target floor number in [1, 9] (which floor you want to go?): \n");
-    target_floor_number = getch() - '0';
+    target_floor_number = getchar() - '0';
 
     if(current_floor_number > target_floor_number)
         return down;
     else if(current_floor_number == target_floor_number)
         return halt;
 }
-int event_atBottom(void){
+static int event_atBottom(void){
     printf("At Bottom! current_floor_number= %d\n", current_floor_number);
     printf("Input target floor number in [1, 9] (which floor you want to go?): \n");
-    target_floor_number = getch() - '0';
+    target_floor_number = getchar() - '0';
 
     if(current_floor_number < target_floor_number)
         return up;
     else if(current_floor_number == target_floor_number)
         return halt;
 }
-int event_malfunction(void){
+static int event_malfunction(void){
     printf("Elevator needs maintanence!\n");
     printf("accumulated_floor_number = %d\n", accumulated_floor_number);
     return quit;
 }
-int event_end(void){
+static int event_end(void){
     printf("Exit!");
     return 0;
 }
-int event_unexpected(void){
+static int event_unexpected(void){
     printf("Debug.");
     return quit;
 }
-
-
-int (* event[])(void) = { event_idle, event_goingUp, event_goingDown, event_atTop, event_atBottom, event_malfunction, event_unexpected, event_end };
 
 // Nice, thanks for this. The only thing possible flaw here is that the lookup_transitions will
 // likely be linear (O(n)) with this transition table datastructure. Its possible to make it better
@@ -130,6 +136,11 @@ int lookup_transitions[][7] = {
 
 int main(){
 
+    char ch = 0;
+    while((ch = getchar()) != EOF) {
+        putchar(ch);
+    }
+
     enum state_codes cur_state = ENTRY_STATE;
     enum ret_codes rc;
     int (* state_func)(void);
@@ -143,8 +154,8 @@ int main(){
         cur_state = lookup_transitions[cur_state][rc];
     }
     printf("END.");
-    getch();
-    getch();
-    getch();
+    getchar();
+    getchar();
+    getchar();
     return 0;
 }
